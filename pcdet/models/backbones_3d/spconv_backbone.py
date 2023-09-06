96c55f691448af40fddfab3ca8f3319568f8d51b
CharlesHAO <hcheng1005@gmail.com>
2023-09-06 23:36:27 +0800

    新增系数卷积模型导出

--- pcdet/models/backbones_3d/spconv_backbone.py
+++ pcdet/models/backbones_3d/spconv_backbone.py
@@ -68,9 +68,10 @@ class SparseBasicBlock(spconv.SparseModule):
 
 
 class VoxelBackBone8x(nn.Module):
-    def __init__(self, model_cfg, input_channels, grid_size, **kwargs):
+    def __init__(self, input_channels, grid_size, **kwargs):
         super().__init__()
-        self.model_cfg = model_cfg
+        # 此处修改是为了生成scn临时屏蔽
+        # self.model_cfg = model_cfg
         norm_fn = partial(nn.BatchNorm1d, eps=1e-3, momentum=0.01)
 
         self.sparse_shape = grid_size[::-1] + [1, 0, 0]
@@ -108,7 +109,8 @@ class VoxelBackBone8x(nn.Module):
         )
 
         last_pad = 0
-        last_pad = self.model_cfg.get('last_pad', last_pad)
+         # 此处修改是为了生成scn临时屏蔽
+        # last_pad = self.model_cfg.get('last_pad', last_pad)
         self.conv_out = spconv.SparseSequential(
             # [200, 150, 5] -> [200, 150, 2]
             spconv.SparseConv3d(64, 128, (3, 1, 1), stride=(2, 1, 1), padding=last_pad,
