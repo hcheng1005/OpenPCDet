320370a1302744a9ef822e00442a2a9452353571
CharlesCH <hcheng1005@gmail.com>
2023-10-11 22:40:46 +0800

    update

---
+++ tools/mynuScenes2ros.py
@@ -0,0 +1,412 @@
+import argparse
+import glob
+from pathlib import Path
+
+import os
+import numpy as np
+import argparse
+
+import cv2
+import torch
+
+from pcdet.config import cfg, cfg_from_yaml_file
+from pcdet.datasets import DatasetTemplate
+from pcdet.models import build_network, load_data_to_gpu
+from pcdet.utils import common_utils
+
+from nuscenes import NuScenes
+import time
+import open3d
+from visual_utils import open3d_vis_utils as V
+
+# ros
+import rospy
+from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import PointField
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
+
+from scipy.spatial.transform import Rotation as R
+
+import numpy as np
+from std_msgs.msg import Header
+
+lines = [[0, 1], [1, 2], [2, 3], [3, 0], [4, 5], [5, 6],
+         [6, 7], [7, 4], [0, 4], [1, 5], [2, 6], [3, 7]]
+
+
+class Detector:
+    def __init__(self) -> None:
+        self.marker_pub = rospy.Publisher(
+            '/detect_box3d', MarkerArray, queue_size=2)
+
+        self.marker_array = MarkerArray()
+
+    def rotx(self, t):
+        c = np.cos(t)
+        s = np.sin(t)
+        return np.array([[1,  0,  0],
+                        [0,  c,  -s],
+                        [0, s,  c]])
+
+    def roty(self, t):
+        c = np.cos(t)
+        s = np.sin(t)
+        return np.array([[c,  0,  s],
+                        [0,  1,  0],
+                        [-s, 0,  c]])
+
+    def rotz(self, t):
+        c = np.cos(t)
+        s = np.sin(t)
+        return np.array([[c,  -s,  0],
+                        [s,  c,  0],
+                        [0, 0,  1]])
+
+    def get_3d_box(self, center, box_size, heading_angle):
+        ''' Calculate 3D bounding box corners from its parameterization.
+
+        Input:heading_angle
+            box_size: tuple of (l,w,h)
+            : rad scalar, clockwise from pos z axis
+            center: tuple of (x,y,z)
+        Output:
+            corners_3d: numpy array of shape (8,3) for 3D box cornders
+        '''
+        R = self.rotz(heading_angle)
+        l, w, h = box_size
+        x_corners = [l/2, l/2, -l/2, -l/2, l/2, l/2, -l/2, -l/2]
+        y_corners = [w/2, -w/2, -w/2, w/2, w/2, -w/2, -w/2, w/2]
+        z_corners = [h/2, h/2, h/2, h/2, -h/2, -h/2, -h/2, -h/2]
+        corners_3d = np.dot(R, np.vstack([x_corners, y_corners, z_corners]))
+        corners_3d[0, :] = corners_3d[0, :] + center[0]
+        corners_3d[1, :] = corners_3d[1, :] + center[1]
+        corners_3d[2, :] = corners_3d[2, :] + center[2]
+        corners_3d = np.transpose(corners_3d)
+        return corners_3d
+
+    def display(self, boxes):
+        self.marker_array.markers.clear()
+
+        for obid in range(len(boxes)):
+            ob = boxes[obid]
+            tid = 0
+            detect_points_set = []
+            for i in range(0, 8):
+                detect_points_set.append(Point(ob[i], ob[i+8], ob[i+16]))
+
+            marker = Marker()
+            marker.header.frame_id = 'nuscenes'
+            marker.header.stamp = rospy.Time.now()
+
+            marker.id = obid*2
+            marker.action = Marker.ADD
+            marker.type = Marker.LINE_LIST
+
+            marker.lifetime = rospy.Duration(1)
+
+            marker.color.r = 1
+            marker.color.g = 0
+            marker.color.b = 0
+
+            marker.color.a = 1
+            marker.scale.x = 0.2
+            marker.points = []
+
+            for line in lines:
+                marker.points.append(detect_points_set[line[0]])
+                marker.points.append(detect_points_set[line[1]])
+
+        self.marker_array.markers.append(marker)
+
+        self.marker_pub.publish(self.marker_array)
+
+
+class DemoDataset(DatasetTemplate):
+    def __init__(self, dataset_cfg, class_names, training=True, root_path=None, logger=None, ext='.bin'):
+        """
+        Args:
+            root_path:
+            dataset_cfg:
+            class_names:
+            training:
+            logger:
+        """
+        super().__init__(
+            dataset_cfg=dataset_cfg, class_names=class_names, training=training, root_path=root_path, logger=logger
+        )
+        self.root_path = root_path
+        self.ext = ext
+        data_file_list = glob.glob(
+            str(root_path / f'*{self.ext}')) if self.root_path.is_dir() else [self.root_path]
+
+        data_file_list.sort()
+        self.sample_file_list = data_file_list
+
+    def __len__(self):
+        return len(self.sample_file_list)
+
+    def __getitem__(self, index):
+        points = np.fromfile(
+            self.sample_file_list[index], dtype=np.float32, count=-1).reshape([-1, 5])[:, :4]
+
+        sweep_points_list = [points]
+        sweep_times_list = [np.zeros((points.shape[0], 1))]
+
+        points = np.concatenate(sweep_points_list, axis=0)
+        times = np.concatenate(sweep_times_list, axis=0).astype(points.dtype)
+
+        points = np.concatenate((points, times), axis=1)
+
+        input_dict = {
+            'points': points,
+            'frame_id': times,
+        }
+
+        data_dict = self.prepare_data(data_dict=input_dict)
+        return data_dict
+
+    def gen_data_dict(self, file_path):
+        points = np.fromfile(file_path, dtype=np.float32,
+                             count=-1).reshape([-1, 5])[:, :4]
+        sweep_points_list = [points]
+        sweep_times_list = [np.zeros((points.shape[0], 1))]
+
+        points = np.concatenate(sweep_points_list, axis=0)
+        times = np.concatenate(sweep_times_list, axis=0).astype(points.dtype)
+
+        points = np.concatenate((points, times), axis=1)
+
+        input_dict = {
+            'points': points,
+            'frame_id': times,
+        }
+
+        data_dict = self.prepare_data(data_dict=input_dict)
+        return data_dict
+
+
+def parse_config():
+    data_path = "/media/charles/ShareDisk/00myDataSet/nuScenes/v1.0-mini/sweeps/LIDAR_TOP/"
+    cfg_file = "cfgs/nuscenes_models/cbgs_voxel01_res3d_centerpoint.yaml"
+    ckpt = "ckpt/nuScenes/cbgs_voxel01_centerpoint_nds_6454.pth"
+
+    parser = argparse.ArgumentParser(description='arg parser')
+    parser.add_argument('--cfg_file', type=str, default=cfg_file,
+                        help='specify the config for demo')
+    parser.add_argument('--data_path', type=str, default=data_path,
+                        help='specify the point cloud data file or directory')
+    parser.add_argument('--ckpt', type=str, default=ckpt,
+                        help='specify the pretrained model')
+    parser.add_argument('--ext', type=str, default='.bin',
+                        help='specify the extension of your point cloud data file')
+
+    args = parser.parse_args()
+
+    cfg_from_yaml_file(args.cfg_file, cfg)
+
+    return args, cfg
+
+
+def pub_3d_bbox2(box_pub, pred_dicts):
+    detector = Detector()
+    if isinstance(pred_dicts, torch.Tensor):
+        pred_dicts = pred_dicts.cpu().numpy()
+
+    print("det numb is ", pred_dicts.shape[0])
+    boxes = []
+    for i in range(pred_dicts.shape[0]):
+        box = detector.get_3d_box(
+            pred_dicts[i][0:3], pred_dicts[i][3:6], pred_dicts[i][6])
+        box = box.transpose(1, 0).ravel()
+        boxes.append(box)
+
+    detector.display(boxes)
+
+
+def translate_boxes_to_open3d_instance(gt_boxes):
+    """
+             4-------- 6
+           /|         /|
+          5 -------- 3 .
+          | |        | |
+          . 7 -------- 1
+          |/         |/
+          2 -------- 0
+    """
+    center = gt_boxes[0:3]
+    lwh = gt_boxes[3:6]
+    axis_angles = np.array([0, 0, gt_boxes[6] + 1e-10])
+    rot = open3d.geometry.get_rotation_matrix_from_axis_angle(axis_angles)
+    box3d = open3d.geometry.OrientedBoundingBox(center, rot, lwh)
+
+    line_set = open3d.geometry.LineSet.create_from_oriented_bounding_box(box3d)
+
+    # import ipdb; ipdb.set_trace(context=20)
+    lines = np.asarray(line_set.lines)
+    lines = np.concatenate([lines, np.array([[1, 4], [7, 6]])], axis=0)
+
+    line_set.lines = open3d.utility.Vector2iVector(lines)
+
+    return lines, box3d
+
+
+def pub_3d_bbox(box_pub, pred_dicts, pred_labels):
+    bboxes = MarkerArray()
+
+    if isinstance(pred_dicts, torch.Tensor):
+        pred_dicts = pred_dicts.cpu().numpy()
+
+    print("det numb is ", pred_dicts.shape[0])
+    for i in range(pred_dicts.shape[0]):
+        # line_set, box3d = translate_boxes_to_open3d_instance(pred_dicts[i])
+
+        if pred_labels[i] < 3:
+
+            marker = Marker()
+            marker.id = i
+            marker.header.frame_id = "nuscenes"  # 设置坐标系
+            marker.type = Marker.CUBE  # 指定标记类型为立方体
+            marker.action = Marker.ADD
+            marker.scale.x = pred_dicts[i][3]  # 立方体的尺寸
+            marker.scale.y = pred_dicts[i][4]
+            marker.scale.z = pred_dicts[i][5]
+            marker.color.a = 0.3  # 不透明
+            marker.color.r = 0.0  # 红色
+            marker.color.g = 1.0  # 红色
+            marker.color.b = 0.0  # 红色
+
+            r = R.from_euler('xyz', [0, 0, pred_dicts[i][6]])
+
+            marker.pose.orientation.x = r.as_quat()[0]
+            marker.pose.orientation.y = r.as_quat()[1]
+            marker.pose.orientation.z = r.as_quat()[2]
+            marker.pose.orientation.w = r.as_quat()[3]
+
+            marker.lifetime = rospy.Duration(1.0)  # 显示时间
+
+            # 设置立方体的位置
+            marker.pose.position.x = pred_dicts[i][0]
+            marker.pose.position.y = pred_dicts[i][1]
+            marker.pose.position.z = pred_dicts[i][2]
+
+            bboxes.markers.append(marker)
+
+    box_pub.publish(bboxes)
+
+
+def main():
+    args, cfg = parse_config()
+    logger = common_utils.create_logger()
+    logger.info(
+        '-----------------Quick nuScenes Demo of OpenPCDet-------------------------')
+
+    # 加载数据集
+    demo_dataset = DemoDataset(
+        dataset_cfg=cfg.DATA_CONFIG,
+        class_names=cfg.CLASS_NAMES,
+        training=False,
+        root_path=Path(args.data_path),  logger=logger
+    )
+
+    logger.info(f'Total number of samples: \t{len(demo_dataset)}')
+
+    # 加载摄像头数据
+    img_path = "/media/charles/ShareDisk/00myDataSet/nuScenes/v1.0-trainval/sweeps/CAM_FRONT/"
+    img_file_list = glob.glob(str(Path(img_path) / f'*.jpg'))
+    img_file_list.sort()
+
+    # 加载模型
+    model = build_network(model_cfg=cfg.MODEL, num_class=len(
+        cfg.CLASS_NAMES), dataset=demo_dataset)
+
+    # 导入模型数据
+    model.load_params_from_file(
+        filename=args.ckpt, logger=logger, to_cpu=False)
+    model.cuda()
+    model.eval()
+
+    nusc = NuScenes(version='v1.0-mini',
+                    dataroot='/media/charles/ShareDisk/00myDataSet/nuScenes/v1.0-mini/',
+                    verbose=True)
+
+    pub = rospy.Publisher('pointcloud_topic', PointCloud2, queue_size=5)
+    rospy.init_node('pointcloud_publisher_node', anonymous=True)
+    rate = rospy.Rate(1)
+    msg = PointCloud2()
+    msg.header.stamp = rospy.Time().now()
+    msg.header.frame_id = "nuscenes"
+
+    ros_img = Image()
+    pub_img = rospy.Publisher("/front_camera/image_raw", Image, queue_size=2)
+
+    box_pub = rospy.Publisher('visualization_marker',
+                              MarkerArray, queue_size=10)
+
+    with torch.no_grad():
+        test_sersor = 'LIDAR_TOP'
+        for scene_idx in range(len(nusc.scene)):
+            scene = nusc.scene[scene_idx]
+
+            # 获取该场景的first token
+            cur_sample_info = nusc.get('sample', scene['first_sample_token'])
+
+            # 连续读取该scenes下的所有frame
+            while cur_sample_info['next'] != "":
+                path_ = nusc.get_sample_data_path(
+                    cur_sample_info['data'][test_sersor])
+                # print(path_)
+                data_dict = demo_dataset.gen_data_dict(path_)
+                data_dict = demo_dataset.collate_batch([data_dict])
+                load_data_to_gpu(data_dict)
+
+                pred_dicts, _ = model.forward(data_dict)
+
+                # 读取camera
+                img_path_ = nusc.get_sample_data_path(
+                    cur_sample_info['data']['CAM_FRONT'])
+                image = cv2.imread(img_path_)
+                header = Header(stamp=rospy.Time.now())
+                header.frame_id = 'nuscenes'
+                ros_img.encoding = 'rgb8'
+                ros_img.header = header
+                ros_img.height = image.shape[0]
+                ros_img.width = image.shape[1]
+                ros_img.step = image.shape[1] * image.shape[2]
+                ros_img.data = np.array(image).tostring()
+
+                # 读取lidar数据
+                points = data_dict['points'][:, 1:]
+                if isinstance(points, torch.Tensor):
+                    points = points.cpu().numpy()
+                points = points[:, :3]
+                msg.height = 1
+                msg.width = len(points)
+                print(len(points))
+                msg.fields = [PointField('x', 0, PointField.FLOAT32, 1),
+                              PointField('y', 4, PointField.FLOAT32, 1),
+                              PointField('z', 8, PointField.FLOAT32, 1)]
+                msg.is_bigendian = False
+                msg.point_step = 12
+                msg.row_step = msg.point_step * points.shape[0]
+                msg.is_dense = False
+                msg.data = np.asarray(points, np.float32).tostring()
+
+                # 发布lidar
+                pub.publish(msg)
+                # 发布camera
+                pub_img.publish(ros_img)
+
+                pub_3d_bbox(
+                    box_pub, pred_dicts[0]['pred_boxes'], pred_dicts[0]['pred_labels'])
+
+                rate.sleep()
+
+                # next
+                cur_sample_info = nusc.get('sample', cur_sample_info['next'])
+
+
+if __name__ == '__main__':
+    main()
