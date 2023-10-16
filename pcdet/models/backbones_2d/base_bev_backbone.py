9b9a6dfdbb44c0146bc3a6c3a31bcb79cedbe265
CharlesCH <hcheng1005@gmail.com>
2023-10-16 22:29:59 +0800

    add YOLOX

--- pcdet/models/backbones_2d/base_bev_backbone.py
+++ pcdet/models/backbones_2d/base_bev_backbone.py
@@ -78,45 +78,21 @@ class BaseBEVBackbone(nn.Module):
 
         self.num_bev_features = c_in
 
-    # def forward(self, data_dict):
-    #     """
-    #     Args:
-    #         data_dict:
-    #             spatial_features
-    #     Returns:
-    #     """
-    #     spatial_features = data_dict['spatial_features']        
-    #     ups = []
-    #     ret_dict = {}
-    #     x = spatial_features
-    #     for i in range(len(self.blocks)):
-    #         x = self.blocks[i](x)
-    #         stride = int(spatial_features.shape[2] / x.shape[2])
-    #         ret_dict['spatial_features_%dx' % stride] = x
-    #         if len(self.deblocks) > 0:
-    #             ups.append(self.deblocks[i](x))
-    #         else:
-    #             ups.append(x)
-
-    #     if len(ups) > 1:
-    #         x = torch.cat(ups, dim=1)
-    #     elif len(ups) == 1:
-    #         x = ups[0]
-
-    #     if len(self.deblocks) > len(self.blocks):
-    #         x = self.deblocks[-1](x)
-
-    #     data_dict['spatial_features_2d'] = x
-        
-    #     print(x.shape)
-
-    #     return data_dict
-    
-    def forward(self, spatial_features):      
+    def forward(self, data_dict):
+        """
+        Args:
+            data_dict:
+                spatial_features
+        Returns:
+        """
+        spatial_features = data_dict['spatial_features']        
         ups = []
+        ret_dict = {}
         x = spatial_features
         for i in range(len(self.blocks)):
             x = self.blocks[i](x)
+            stride = int(spatial_features.shape[2] / x.shape[2])
+            ret_dict['spatial_features_%dx' % stride] = x
             if len(self.deblocks) > 0:
                 ups.append(self.deblocks[i](x))
             else:
@@ -130,7 +106,31 @@ class BaseBEVBackbone(nn.Module):
         if len(self.deblocks) > len(self.blocks):
             x = self.deblocks[-1](x)
 
-        return x
+        data_dict['spatial_features_2d'] = x
+        
+        print(x.shape)
+
+        return data_dict
+    
+    # def forward(self, spatial_features):      
+    #     ups = []
+    #     x = spatial_features
+    #     for i in range(len(self.blocks)):
+    #         x = self.blocks[i](x)
+    #         if len(self.deblocks) > 0:
+    #             ups.append(self.deblocks[i](x))
+    #         else:
+    #             ups.append(x)
+
+    #     if len(ups) > 1:
+    #         x = torch.cat(ups, dim=1)
+    #     elif len(ups) == 1:
+    #         x = ups[0]
+
+    #     if len(self.deblocks) > len(self.blocks):
+    #         x = self.deblocks[-1](x)
+
+    #     return x
 
 
 class BaseBEVBackboneV1(nn.Module):
--- tools/mynuScenes2ros.py
+++ tools/mynuScenes2ros.py
@@ -33,6 +33,150 @@ from scipy.spatial.transform import Rotation as R
 import numpy as np
 from std_msgs.msg import Header
 
+
+import argparse
+import os
+import time
+from loguru import logger
+
+import cv2
+
+import torch
+
+from yolox.data.data_augment import ValTransform
+from yolox.data.datasets import COCO_CLASSES
+from yolox.exp import get_exp
+from yolox.utils import fuse_model, get_model_info, postprocess, vis
+
+IMAGE_EXT = [".jpg", ".jpeg", ".webp", ".bmp", ".png"]
+
+
+def make_parser():
+    parser = argparse.ArgumentParser("YOLOX Demo!")
+    parser.add_argument("-expn", "--experiment-name", type=str, default=None)
+    parser.add_argument("-n", "--name", type=str, default="yolox-m", help="model name")
+
+    parser.add_argument(
+        "--path", default="/media/charles/ShareDisk/00myDataSet/nuScenes/v1.0-mini/sweeps/CAM_FRONT", help="path to images or video"
+    )
+    parser.add_argument("--camid", type=int, default=0, help="webcam demo camera id")
+    parser.add_argument(
+        "--save_result",
+        action="store_true",
+        help="whether to save the inference result of image/video",
+    )
+
+    # exp file
+    parser.add_argument(
+        "-f",
+        "--exp_file",
+        default=None,
+        type=str,
+        help="please input your experiment description file",
+    )
+    parser.add_argument("-c", "--ckpt", default="/home/charles/myCode/about_camera/YOLOX/pth/yolox_m.pth", type=str, help="ckpt for eval")
+    parser.add_argument("--conf", default=0.3, type=float, help="test conf")
+    parser.add_argument("--nms", default=0.3, type=float, help="test nms threshold")
+    parser.add_argument("--tsize", default=640, type=int, help="test img size")
+    parser.add_argument(
+        "--fuse",
+        dest="fuse",
+        default=False,
+        action="store_true",
+        help="Fuse conv and bn for testing.",
+    )
+
+    return parser
+
+
+def get_image_list(path):
+    image_names = []
+    for maindir, subdir, file_name_list in os.walk(path):
+        for filename in file_name_list:
+            apath = os.path.join(maindir, filename)
+            ext = os.path.splitext(apath)[1]
+            if ext in IMAGE_EXT:
+                image_names.append(apath)
+    return image_names
+
+
+class Predictor(object):
+    def __init__(
+        self,
+        model,
+        exp,
+        cls_names=COCO_CLASSES,
+        trt_file=None,
+        decoder=None,
+        device="gpu",
+        fp16=False,
+        legacy=False,
+    ):
+        self.model = model
+        self.cls_names = cls_names
+        self.decoder = decoder
+        self.num_classes = exp.num_classes
+        self.confthre = exp.test_conf
+        self.nmsthre = exp.nmsthre
+        self.test_size = exp.test_size
+        self.device = device
+        self.fp16 = fp16
+        self.preproc = ValTransform(legacy=legacy)
+
+    def inference(self, img):
+        img_info = {"id": 0}
+        if isinstance(img, str):
+            img_info["file_name"] = os.path.basename(img)
+            img = cv2.imread(img)
+        else:
+            img_info["file_name"] = None
+
+        height, width = img.shape[:2]
+        img_info["height"] = height
+        img_info["width"] = width
+        img_info["raw_img"] = img
+
+        ratio = min(self.test_size[0] / img.shape[0], self.test_size[1] / img.shape[1])
+        img_info["ratio"] = ratio
+
+        img, _ = self.preproc(img, None, self.test_size)
+        img = torch.from_numpy(img).unsqueeze(0)
+        img = img.float()
+        if self.device == "gpu":
+            img = img.cuda()
+            if self.fp16:
+                img = img.half()  # to FP16
+
+        with torch.no_grad():
+            t0 = time.time()
+            outputs = self.model(img)
+            if self.decoder is not None:
+                outputs = self.decoder(outputs, dtype=outputs.type())
+            outputs = postprocess(
+                outputs, self.num_classes, self.confthre,
+                self.nmsthre, class_agnostic=True
+            )
+            logger.info("Infer time: {:.4f}s".format(time.time() - t0))
+        return outputs, img_info
+
+    def visual(self, output, img_info, cls_conf=0.35):
+        ratio = img_info["ratio"]
+        img = img_info["raw_img"]
+        if output is None:
+            return img
+        output = output.cpu()
+
+        bboxes = output[:, 0:4]
+
+        # preprocessing: resize
+        bboxes /= ratio
+
+        cls = output[:, 6]
+        scores = output[:, 4] * output[:, 5]
+
+        vis_res = vis(img, bboxes, scores, cls, cls_conf, self.cls_names)
+        return vis_res
+
 lines = [[0, 1], [1, 2], [2, 3], [3, 0], [4, 5], [5, 6],
          [6, 7], [7, 4], [0, 4], [1, 5], [2, 6], [3, 7]]
 
@@ -296,7 +440,54 @@ def pub_3d_bbox(box_pub, pred_dicts, pred_labels):
 
     box_pub.publish(bboxes)
 
+def YOLOX_main(exp, args):
+    if not args.experiment_name:
+        args.experiment_name = exp.exp_name
 
+    file_name = os.path.join(exp.output_dir, args.experiment_name)
+    os.makedirs(file_name, exist_ok=True)
+
+    logger.info("Args: {}".format(args))
+
+    if args.conf is not None:
+        exp.test_conf = args.conf
+    if args.nms is not None:
+        exp.nmsthre = args.nms
+    if args.tsize is not None:
+        exp.test_size = (args.tsize, args.tsize)
+
+    model = exp.get_model()
+    logger.info("Model Summary: {}".format(get_model_info(model, exp.test_size)))
+    model.cuda()
+    model.eval()
+
+    if args.ckpt is None:
+        ckpt_file = os.path.join(file_name, "best_ckpt.pth")
+    else:
+        ckpt_file = args.ckpt
+    logger.info("loading checkpoint")
+    ckpt = torch.load(ckpt_file, map_location="cpu")
+    # load the model state dict
+    model.load_state_dict(ckpt["model"])
+    logger.info("loaded checkpoint done.")
+
+    if args.fuse:
+        logger.info("\tFusing model...")
+        model = fuse_model(model)
+
+    predictor = Predictor(model, exp, COCO_CLASSES)
+    return predictor
+    # if os.path.isdir(args.path):
+    #     files = get_image_list(args.path)
+    # else:
+    #     files = [args.path]
+    # files.sort()
+    # for image_name in files:
+    #     outputs, img_info = predictor.inference(image_name)
+    #     result_image = predictor.visual(outputs[0], img_info, predictor.confthre)
+    #     cv2.imshow('demo', result_image)
+    #     cv2.waitKey(20)
+        
 def main():
     args, cfg = parse_config()
     logger = common_utils.create_logger()
@@ -344,7 +535,12 @@ def main():
 
     box_pub = rospy.Publisher('visualization_marker',
                               MarkerArray, queue_size=10)
-
+    
+    # 加载摄像头模型
+    args = make_parser().parse_args()
+    exp = get_exp(args.exp_file, args.name)
+    yolox_predictor = YOLOX_main(exp, args)
+    
     with torch.no_grad():
         test_sersor = 'LIDAR_TOP'
         for scene_idx in range(len(nusc.scene)):
@@ -367,15 +563,18 @@ def main():
                 # 读取camera
                 img_path_ = nusc.get_sample_data_path(
                     cur_sample_info['data']['CAM_FRONT'])
-                image = cv2.imread(img_path_)
+                
+                outputs, img_info = yolox_predictor.inference(img_path_)
+                result_image = yolox_predictor.visual(outputs[0], img_info, yolox_predictor.confthre)
+                # image = cv2.imread(img_path_)
                 header = Header(stamp=rospy.Time.now())
                 header.frame_id = 'nuscenes'
-                ros_img.encoding = 'rgb8'
+                ros_img.encoding = 'bgr8'
                 ros_img.header = header
-                ros_img.height = image.shape[0]
-                ros_img.width = image.shape[1]
-                ros_img.step = image.shape[1] * image.shape[2]
-                ros_img.data = np.array(image).tostring()
+                ros_img.height = result_image.shape[0]
+                ros_img.width = result_image.shape[1]
+                ros_img.step = result_image.shape[1] * result_image.shape[2]
+                ros_img.data = np.array(result_image).tostring()
 
                 # 读取lidar数据
                 points = data_dict['points'][:, 1:]
